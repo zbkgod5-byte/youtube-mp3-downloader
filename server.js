26de93cc15b1abfc7a375508e85b8621998fcc88
zbkgod5-byte <zbkgod5@gmail.com>
2026-04-15 13:25:05 +0700

    Update code for Render deployment

--- server.js
+++ server.js
@@ -50,4 +50,9 @@ app.get('/get-file', (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log('🚀 Server រត់លើ http://localhost:3000'));
+// កូដដែលត្រឹមត្រូវសម្រាប់ Cloud
+const PORT = process.env.PORT || 3000; 
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`🚀 Server is running on port ${PORT}`);
+});
